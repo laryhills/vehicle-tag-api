d24ee77e3419f218eff03e1f28830c95a93bb1be
Lary Hills <hilaryokoh@gmail.com>
2022-05-18 22:35:20 +0100

    Trial 1 : upgraded composer

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
+
+echo "Upgrading Composer"
+composer self-update --2
+
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
+#echo "Caching config..."
+#php artisan config:cache
+#
+#echo "Caching routes..."
+#php artisan route:cache
 
 #echo "Running migrations..."
 #php artisan migrate --force
+
+echo "Running serve"
+php artisan serve
